68ccd3fdc43e7f2f2600262d8f72d8b1da9dccad
Ivanshamir <shamirimtiaz.ice@gmail.com>
2019-09-11 00:49:19 +0600

    post login data and catch the response

--- resources/js/components/auth/login.vue
+++ resources/js/components/auth/login.vue
@@ -3,14 +3,14 @@
         <div class="card card-login mx-auto mt-5">
             <div class="card-header">Login</div>
             <div class="card-body">
-                <form>
+                <form @submit.prevent="login">
                 <div class="form-group">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input class="form-control" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="Enter email">
+                    <input class="form-control" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="Enter email" v-model="form.email">
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password">
+                    <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password" v-model="form.password">
                 </div>
                 <button class="btn btn-primary btn-block" type="submit">Login</button>
                 </form>
@@ -25,6 +25,22 @@
 
 <script>
     export default {
+        data(){
+            return{
+                form:{
+                    email: '',
+                    password: ''
+                }
+            }
+        },
+        methods:{
+            login(){
+                axios.post('/api/auth/login', this.form)
+                .then(res => console.log(res.data))
+                .catch(e => console.log(e.response.data))
+
+            }
+        },
         mounted() {
             // console.log('Component mounted.')
         }
